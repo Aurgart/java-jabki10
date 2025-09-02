2ff5d8acae1f0540cc56834876fc671a9dee6d76
Aurgart <ilyalemeshew@gmail.com>
2025-09-02 15:58:34 +0300

    Инит домашки 10

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import user_exceptions.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main {
+    // логины
+    static HashMap<String, String> logins = new HashMap<>();
+    // продукты
+    static List<Item> itemList = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+    }
+
+}
